60c38eaed31babccc5c349494344be39dd52206f
leonardogloria <contato@leonardogloria.com.br>
2022-03-30 21:47:01 -0300

    Adicionando notificacao

--- src/test/java/br/com/infinet/motorservice/MotorServiceApplicationTests.java
+++ src/test/java/br/com/infinet/motorservice/MotorServiceApplicationTests.java
@@ -2,12 +2,13 @@ package br.com.infinet.motorservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class MotorServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(false);
 	}
 
 }
